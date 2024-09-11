f7a463978aed76610b59cf3f1925f98e8138ba36
George Lam <georgelamd@gmail.com>
2024-09-11 15:58:11 -0400

    Imitate batch insertions

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python
-RUN pip install pandas psycopg2 sqlalchemy
+RUN pip install confluent-kafka pandas psycopg2 sqlalchemy
 COPY /src/extract.py /src/
 COPY /data/owid-covid-data.csv /data/
 CMD ["python", "-u", "/src/extract.py"]
--- src/extract.py
+++ src/extract.py
@@ -4,7 +4,9 @@
 # In[1]:
 
 
+import random
 import time
+
 import pandas as pd
 
 from sqlalchemy import create_engine
@@ -14,29 +16,51 @@ from sqlalchemy import create_engine
 
 
 MAX_RETRIES = 5
-SLEEP_TIME = 1
 
 
 # In[3]:
 
 
+print("establishing connection...")
 retry = 1
 while retry <= MAX_RETRIES:
     try:
         engine = create_engine("postgresql://root:wordpass@database:5432/covid")
         engine.connect()
+        print("connection established")
         break
     except:
         if retry == MAX_RETRIES:
             raise Exception("Database connection failed.")
         retry += 1
-        time.sleep(SLEEP_TIME)
+        time.sleep(1)
 
 
-# In[4]:
+# In[ ]:
 
 
+print("reading csv into master dataframe...")
 df = pd.read_csv("../data/owid-covid-data.csv")
-df_deaths = df[["location", "date", "total_cases", "new_cases", "total_deaths", "population"]]
-df_deaths.to_sql(con=engine, name="deaths")
+df = df[["continent", "location", "date", "new_cases", "total_cases", "new_deaths", "total_deaths", "reproduction_rate", "population", "new_vaccinations", "total_vaccinations", "median_age", "aged_65_older", "aged_70_older", "life_expectancy"]]
+df = df.sort_values(by="date")
+print("reading complete")
+
+
+# In[ ]:
+
+
+print("imitating database insertions...")
+index = 0
+batch_size = random.randint(1, 5)
+batch_df = pd.DataFrame()
+while index < len(df):
+    batch_df = pd.concat([batch_df, df.iloc[[index]]])
+    if len(batch_df) == batch_size:
+        batch_df.to_sql(con=engine, name="deaths", if_exists="append")
+        print(f"inserted a batch of size {batch_size}")
+        batch_size = random.randint(1, 5)
+        batch_df = pd.DataFrame()
+        time.sleep(5)
+    index += 1
+print("imitation complete")
 
